5f463bcd9c820d437d372182ea4cad2bbe3a7a40
Juampi Badino <juanpablobadino6@gmail.com>
2023-11-18 11:46:52 -0300

    Estilo link al Sports Jam de diciembre 2023

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 
+import './Events.css';
+
 import Container from 'react-bootstrap/Container';
 import { Row, Col } from 'react-bootstrap';
 
-import NavBar from '../components/NavBar/NavBar.tsx';
-
 const Events = () => {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <a href='/Events/DecemberHackJam'>December Hack Jam</a>
-                </Col>
-            </Row>
+        <Container id='events'>
+            <a href='Events/DecemberHackJam'>
+                <img src='/img/sports-jam-diciembre-2023/sports-jam-barqia-onklub.png'
+                    style={{
+                        position: 'absolute',
+                        top: '-50px',
+                        left: '0',
+                        opacity: '0.8',
+                        width: '100%',
+                        zIndex: '0'
+                    }}
+                />
+            </a>
+            
+            <a className='neon' href='/Events/DecemberHackJam'
+                style={{
+                    position: 'absolute',
+                    top: '480px',
+                    left: '360px',
+                    fontSize: '40px'
+                }}>
+                    DICIEMBRE 2023 - Sitio web del evento
+            </a>
         </Container>
     )
 }
