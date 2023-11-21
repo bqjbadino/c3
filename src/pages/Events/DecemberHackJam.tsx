ee52841d96a5462373b9d99965575d3df6748d8a
Juampi Badino <juanpablobadino6@gmail.com>
2023-11-21 11:00:12 -0300

    Sports Jam Diciembre 2023 - Recorte de imágenes

--- src/pages/Events/DecemberHackJam.tsx
+++ src/pages/Events/DecemberHackJam.tsx
@@ -15,9 +15,19 @@ const DecemberHackJam = () => {
     return (
         <div id='december-hack-jam'>
             <NavBar />
-        
+            <div>
+                <img className='hide-on-mobile' src='/img/barqia-sports-jam.jpg' style={{ position: 'absolute', width: '100%', zIndex: '1' }} />
+            </div>
             <Container id='december-hack-jam-text' style={{ background: 'transparent' }}>
                 
+                <h1 style={{ background: 'rgba(0, 0, 0, 0.7)', borderRadius: '10px', fontWeight: 'bold', marginTop: '30px', padding: '15px 0', textAlign: 'center', zIndex: '1' }}>
+                    <span style={{ fontSize: '45px' }}>¡Súmate al <strong>XR HACKATHON SPORTS 2023</strong>!</span>
+                    <hr />
+                    <Countdown />
+                </h1>
+                
+                
+
                 <img className='hide-on-mobile' src='/img/barqia-purple-background-2.jpg' style={{ filter: 'grayscale(80%)', height: '100%', position: 'fixed', top: '0', left: '0' , zIndex: '0'}}/>
                 <Row>
                     {/**
@@ -32,22 +42,16 @@ const DecemberHackJam = () => {
                         {/**
                         <img className='hide-on-desktop' src='/img/barqia-vr-5.jpg' style={{ filter: 'opacity(60%)', position: 'relative', width: '100%', zIndex: '0'}} />
                         */}
-                        <img className='hide-on-mobile' src='/img/barqia-hackjam-2.jpg' />
+                        
                         <img className='hide-on-desktop' src='/img/barqia-hackjam-2.jpg' style={{ marginTop: '135px'}} />
-                        <h2 className='neon hide-on-mobile' style={{ fontSize: '75px', marginTop: '-250px' }}>SPORTS JAM</h2>
-                        <h2 className='neon hide-on-desktop' style={{ fontSize: '30px', marginTop: '-70px' }}>SPORTS JAM</h2>
-                    </Col>
-
-                    <Col sm={12} lg={12} style={{ zIndex: '1'}} >
-                        <Countdown />
+                        
                     </Col>
                     
 
                     <Col sm={12} lg={12} style={{ marginTop: '40px', position: 'relative', zIndex: '1'}}>
                         <div style={{ position: 'relative', zIndex: '1'}}>
-                            <h1 style={{ fontWeight: 'bold', textAlign: 'center' }}>
-                                ¡Súmate al <strong>XR HACKATHON SPORTS 2023</strong>!
-                            </h1>
+                            <h2 className='neon hide-on-mobile' style={{ fontSize: '75px', zIndex: '1'}}>SPORTS JAM</h2>
+                            <h2 className='neon hide-on-desktop' style={{ fontSize: '30px', marginTop: '-70px' }}>SPORTS JAM</h2>
                             <p>
                                 Transformando el mundo del deporte con inteligencia artificial y realidad virtual.
                             </p>
