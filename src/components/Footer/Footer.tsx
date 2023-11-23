fdb98757f770d34eb3220ac0a340978931039855
Juampi Badino <juanpablobadino6@gmail.com>
2023-11-23 12:31:59 -0300

    SJD23 - Ajustes footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,11 +8,11 @@ import Col from 'react-bootstrap/Col';
 
 const Footer = () => {
     return (
-        <Container id='footer' style={{ background: 'black', borderRadius: '10px', padding: '50px', position: 'relative', width: '100%', zIndex: '2'}}>
+        <Container id='footer' style={{ background: 'black', border: '1px solid rgba(256, 256, 256, 0.2)', borderRadius: '10px', padding: '50px', position: 'relative', width: '100%', zIndex: '2'}}>
             <Row>
                 <Col style={{ textAlign: 'center' }}>
-                    <img className='hide-on-mobile' src='/img/barqia-symbol.png' style={{ width: '80px'}} />
-                    <img className='hide-on-mobile' src='/img/barqia-symbol-onklub.png' style={{ filter: 'invert(1)', marginLeft: '-8px', marginTop: '10px', width: '55px'}} />
+                    <img className='hide-on-mobile' src='/img/barqia-symbol.png' style={{ marginRight: '10px', width: '75px'}} />
+                    <img className='hide-on-mobile' src='/img/barqia-symbol-onklub.png' style={{ filter: 'invert(1)', marginLeft: '-8px', marginTop: '10px', width: '50px'}} />
 
                     <img className='hide-on-desktop' src='/img/barqia-symbol.png' style={{ width: '50px'}} />
                     <img className='hide-on-desktop' src='/img/barqia-symbol-onklub.png' style={{ filter: 'invert(1)', marginTop: '10px', width: '30px'}} />
@@ -21,9 +21,16 @@ const Footer = () => {
             <hr style={{ margin: '40px 0'}} />
             <Row id='barqia-social-networks'>
                 <Col style={{ textAlign: 'center' }}>
-                    <a target="_blank" href='https://www.youtube.com/@barqiasports'><img src='/img/logo-youtube.png' /></a>
-                    <a target="_blank" href='https://www.linkedin.com/company/barqiasports/?originalSubdomain=es'><img src='/img/logo-linkedin.png' style={{ margin: '0 40px'}} /></a>
-                    <a target="_blank" href='https://www.instagram.com/barqiasports/'><img src='/img/logo-instagram.png' /></a>
+                    <a target="_blank" href='https://www.youtube.com/@barqiasports'><img src='/img/icon-youtube.png' style={{ filter: 'brightness(0) invert(1)', width: '37x' }} /></a>
+                    <a target="_blank" href='https://www.linkedin.com/company/barqiasports/?originalSubdomain=es'><img src='/img/icon-linkedin.png' style={{ filter: 'brightness(0) invert(1)', margin: '0 40px', marginTop: '-10px', width: '37px'}}  /></a>
+                    <a target="_blank" href='https://www.instagram.com/barqiasports/'><img src='/img/icon-instagram.png' style={{ filter: 'brightness(0) invert(1)', width: '37px' }} /></a>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p style={{ fontSize: '15px', marginTop: '45px', textAlign: 'center' }}>
+                        2023 Barqia - All Rights Reserved.
+                    </p>
                 </Col>
             </Row>
         </Container>
